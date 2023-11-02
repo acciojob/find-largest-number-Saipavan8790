f5491f8e6961421e48c6bf1990355b6d255221c8
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-11-02 13:41:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+// function findLargest(a, b, c) {
+//   //your code here
+// 	if (a>b)
+// 	{
+// 		if(a>c) return a;
+// 		else return c;
+// 	}
+// 	if(b>a)
+// 	{
+// 		if(b>c) return b;
+// 		else return c;
+// 	}
+// 	if(c>a)
+// 	{
+// 		if(c>b) return c;
+// 		else return b;
+// 	}
+// }
 function findLargest(a, b, c) {
   //your code here
-	if (a>b)
-	{
-		if(a>c) return a;
-		else return c;
-	}
-	if(b>a)
-	{
-		if(b>c) return b;
-		else return c;
-	}
-	if(c>a)
-	{
-		if(c>b) return c;
-		else return b;
-	}
+	if(a>b && a>c)
+		return a
+	else if(b>a && b>c)
+		return b
+	else
+		return c
 }
 
 const num1 = parseInt(prompt("Enter First Number."));
 const num2 = parseInt(prompt("Enter Second Number."));
 const num3 = parseInt(prompt("Enter Third Number."));
-// alert(findLargest(num1, num2, num3));
+alert(findLargest(num1, num2, num3));
